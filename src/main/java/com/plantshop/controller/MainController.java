e2bc658ef32ccc3e62a4ecfe31b81d77079d6ecf
Wrmwd <polina_lu03@mail.ru>
2023-04-29 20:20:48 +0300

    Смена пароля для пользователя.

--- src/main/java/com/plantshop/controller/MainController.java
+++ src/main/java/com/plantshop/controller/MainController.java
@@ -1,8 +1,11 @@
 package com.plantshop.controller;
 
 import javax.servlet.http.HttpSession;
+
+import com.plantshop.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,10 +13,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.plantshop.model.UserDtls;
 import com.plantshop.service.UserService;
 
+import java.security.Principal;
+
 @Controller
 public class MainController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserRepository userRepo;
+    @ModelAttribute
+    private void userDetails(Model m, Principal p){
+       if(p!=null){
+           String email = p.getName();
+           UserDtls user = userRepo.findByEmail(email);
+           m.addAttribute("user", user);
+       }
+    }
     @GetMapping("/")
     public String index(){
         return "index";
--- src/main/java/com/plantshop/controller/UserController.java
+++ src/main/java/com/plantshop/controller/UserController.java
@@ -3,11 +3,13 @@ package com.plantshop.controller;
 import com.plantshop.model.UserDtls;
 import com.plantshop.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import javax.swing.text.html.parser.Entity;
 import java.security.Principal;
 
 @Controller
@@ -15,6 +17,8 @@ import java.security.Principal;
 public class UserController {
     @Autowired
     private UserRepository userRepo;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
     @ModelAttribute
     private void userDetails(Model m, Principal p){
         String email = p.getName();
@@ -24,5 +28,29 @@ public class UserController {
     @GetMapping("/")
     public String home(){
         return "user/home";
+    }
+    @GetMapping("/changPass")
+    public String loadChangePassword(){
+        return "user/change_password";
+    }
+    @PostMapping("/updatePassword")
+    public String changePassword(Principal p, @RequestParam("oldPass") String oldPass, @RequestParam("newPass") String newPass, HttpSession session){
+       String email = p.getName();
+       UserDtls loginUser = userRepo.findByEmail(email);
+        boolean f = passwordEncoder.matches(oldPass, loginUser.getPassword());
+        if(f){
+           loginUser.setPassword(passwordEncoder.encode(newPass));
+           UserDtls updatePasswordUser = userRepo.save(loginUser);
+           if(updatePasswordUser!=null){
+               session.setAttribute("msg", "Password Change Successful");
+           }
+           else{
+               session.setAttribute("msg", "Something went wrong on server");
+           }
+        }
+        else{
+            session.setAttribute("msg", "Old password is incorrect");
+        }
+        return "redirect:/user/changPass";
     }
 }
